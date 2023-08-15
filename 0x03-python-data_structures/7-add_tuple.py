f303f8ca8e59cafd09daf4f285f8a9072445cb90
Nkrumah-Dubazana <altonkrumah@gmail.com>
2023-08-15 08:11:46 -0700

    Function that adds two tuples.

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/Python3
+def add_tuple(tuple_a(), tuple_b=()):
+    result_tuple = ()
+    a = (tuple_a + (0, 0))[:2]
+    b = (tuple_b + (0, 0)[:2]
+
+    result_tuple = (a[0] + b[0], a[1] + b[1]
+    return result_tuple
