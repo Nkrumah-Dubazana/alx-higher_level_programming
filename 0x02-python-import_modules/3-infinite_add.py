8f40d661bc23eea790880174cea560b0049211ec
Nkrumah-Dubazana <altonkrumah@gmail.com>
2023-08-16 07:36:15 -0700

    Python function that calculates and prints the result of the addition of all the arguments provided.

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    """Print the addition of all arguments"""
+
+    total_sum = 0
+    for i in range(len(sys.argv) - 1):
+        total_sum += int(sys.argv[i + 1])
+    print("{}".format(total_sum))
