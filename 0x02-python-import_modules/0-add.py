536cb351c893893034d00b0f5b78140ca278a6dc
Nkrumah-Dubazana <altonkrumah@gmail.com>
2023-08-16 04:37:02 -0700

    Addition Function.

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/bin/usr/Python3
+if __name__ == "__main__":
+    """Add and Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} +{} = {}.format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/Python3
+def add(a, b):
+    """Addition Function
+
+    Args:
+         a: Num 1
+         b: Num 2
+
+    Returns:
+         Return a+b
+    """
+    return (a+b)
