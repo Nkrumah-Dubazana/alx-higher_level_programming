f255d0769c3f2c13a18b537825bb3ee972276371
Nkrumah-Dubazana <altonkrumah@gmail.com>
2023-08-16 07:38:18 -0700

    Function that prints the number of arguments and list of arguments, along with thier positions.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-import sys
-if __name__ = "__main__":
+if __name__ == "__main__":
+    """Print the number of and list of arguments"""
+    import sys
 
-    num_arguments = len(sys.argv) - 1
-    if num_arguments == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif num_argemnts == 1:
-        print("1 arguments:")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{}: {}".format(i +1, sys.argv[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
