d723e7266b4658d26dc67979758a2f1eca29ae0a
Nkrumah-Dubazana <altonkrumah@gmail.com>
2023-08-18 03:44:49 -0700

    Function that adds all unique integers in a list.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
+
 def search_replace(my_list, search, replace):
-
     """
-    Function that replaces an element of an element by another in a new list
-
+    A function that replaces all occurrences
+    of an element by another in a new list
     """
-
+    new_list = []
     for item in my_list:
         if item == search:
             new_list.append(replace)
         else:
             new_list.append(item)
-
-        return new_list
+    return new_list
 
 
