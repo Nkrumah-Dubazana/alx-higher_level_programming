19c2927043d5edd95139c0cd675ef3d555f71983
Nkrumah-Dubazana <altonkrumah@gmail.com>
2024-03-04 07:42:21 -0800

    0x01-python-if_else_loops_functions.

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Nkrumah Dubazana
+
+def magic_calculation(a, b, c):
+    
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
