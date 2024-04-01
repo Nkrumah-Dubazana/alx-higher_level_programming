12249033847c771cbd130444282be9e013df25c5
Nkrumah-Dubazana <altonkrumah@gmail.com>
2024-04-01 01:11:16 -0700

    0x11-python-network_1.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Script that:
-    Takes in a URL
-    Sends a POST request to passed URL
-    Takes email as a parameter
-    Displays the body of the response
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
 """
 import sys
 import urllib.parse
 import urllib.request
 
-if __name__ = "__main__":
+
+if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
