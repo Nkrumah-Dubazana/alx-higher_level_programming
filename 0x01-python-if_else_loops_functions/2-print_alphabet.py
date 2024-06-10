6dc0e4f3fd60fcc88d230240b15a63f58df24e05
Nkrumah-Dubazana <altonkrumah@gmail.com>
2024-06-10 08:42:47 -0700

    0x01-python-if_else_loops_functions.

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-"""Prints the alphabet in lowercase, not follwed by a new line."""
-
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end = "")
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
