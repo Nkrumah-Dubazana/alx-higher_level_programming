c68ae88e761e9f0e42532ebab401e4193781ce40
Nkrumah-Dubazana <altonkrumah@gmail.com>
2024-06-13 01:46:16 -0700

    0x0E-SQL_more_queries.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- creates the MySQL server user_0d_01 and grants all priviledges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1pwd';
-GRANT ALL PRIVILEDGES ON * . * TO user_0d_1@localhost;
+-- creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,6 @@
---  lists all shows contained in hbtn_0d_tvshows without a genre linked
--- lists all rows of a database that don't have one column
-SELECT tv_shows.title, tv_show_genre_id
-FROM tv_shows LEFT JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- lists all rows of tables in a database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
