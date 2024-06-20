a928c272ddaa6e2e7dc3b5012d14c2200d76a681
Nkrumah-Dubazana <altonkrumah@gmail.com>
2024-06-19 19:08:26 -0700

    0x13-javascript_objects_scopes_closures.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,16 +2,16 @@
 const dict = require('./101-data').dict;
 
 const totalist = Object.entries(dict);
-const vals = Objects.values(dict);
+const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
 for (const j in valsUniq) {
-	const list = [];
-	for (const k in totalist) {
-		if (totallist[k][1] === valsUniq[j]) {
-			list.unshift(totalist[k][0]);
-		}
-	}
-	newDict[valsUniq[j]] = list;
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
 }
 console.log(newDict);
